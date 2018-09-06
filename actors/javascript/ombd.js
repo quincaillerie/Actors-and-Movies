662aeb46b21300fcebf7d48c5dd26bd8d5ca916c
randy <randyaakhus@gmail.com>
2018-09-06 11:48:08 -0700

    Auto stash before merge of "randy" and "origin/randy"

--- actors/javascript/ombd.js
+++ actors/javascript/ombd.js
@@ -49,3 +49,94 @@ $(document).ready(function () {
         
         });
         
+
+
+
+    });
+    var config = {
+        apiKey: "AIzaSyCoKN3aBcomtecZICsL1l_FNfBZawPOzow",
+        authDomain: "movieprojectissofun.firebaseapp.com",
+        databaseURL: "https://movieprojectissofun.firebaseio.com",
+        projectId: "movieprojectissofun",
+        storageBucket: "movieprojectissofun.appspot.com",
+        messagingSenderId: "920966827156"
+    };
+    firebase.initializeApp(config);
+
+
+    var database = firebase.database();
+    var searchArray = []
+
+    $("#submit").click(function (e) {
+        // add things to firebase
+        e.preventDefault();
+        var search = $("#movie-name").val();
+        database.ref().push(search);
+
+    });
+
+    database.ref().on("child_added", async function (childSnapshot) {
+// add our firebase to our array
+        var search2 = childSnapshot.val();
+        console.log(search2)
+        searchArray.push(search2);
+     
+    });
+    setTimeout(function () {
+        count();
+        console.log(searchArray)
+        console.log(newSearch)
+        newSearch.sort(compare)
+        console.log(newSearch)
+        makeTable();
+    }, 2000)
+
+
+    var newSearch = [];
+    function count() {
+
+        var current = null;
+        searchArray.sort();
+        var cnt = 0;
+// make a new array and add times searched into an object
+        for (var i = 0; i <= searchArray.length; i++) {
+
+            if (searchArray[i] != current) {
+
+                if (cnt > 0) {
+
+                    var newObj = {
+                        target: current,
+                        howMany: cnt,
+                    }
+                    newSearch.push(newObj)
+                }
+                current = searchArray[i];
+                cnt = 1;
+
+            } else {
+
+                cnt++;
+
+            }
+        }
+
+    }
+    function compare(a, b) {
+        // sort our new array by how many times things were searched
+        if (a.howMany > b.howMany)
+            return -1;
+        if (a.howMany < b.howMany)
+            return 1;
+        return 0;
+    };
+    //  select recent serach boxes and append newSearch[number].target for amount of seach results you want
+    function makeTable() {
+        for (var i = 0; i < 2; i++) {
+
+            $('#searchResults').append("<tr><td>" + newSearch[i].target + "</td><td>" + newSearch[i].howMany + "</td></tr>")
+            console.log(newSearch)
+        }
+    }
+});
+
